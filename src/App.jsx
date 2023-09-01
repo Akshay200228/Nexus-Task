047b5c644512a5e2eb256fbd995ce867695e777e
Akshay200228 <akshayrs096@gmail.com>
2023-09-01 10:00:35 +0530

    Start working

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Nav from "./components/Nav"
+
+function App() {
+
+  return (
+    <main className='relative z-20 bg-white'>
+      {/* Navbar */}
+      <Nav />
+
+    </main>
+  )
+}
+
+export default App
---
+++ src/assets/icons/index.js
@@ -0,0 +1,7 @@
+import hamburger from './hamburger.svg'
+import close from './close.svg';
+
+export {
+    hamburger,
+    close,
+}
